de20cb929d77e3a8719b769db6cfa363e6302f15
Marcio Lima Inácio <marcio.lima.inacio@gmail.com>
2025-11-05 15:54:38 +0000

    Clean kitty configuration and fix local nvim search

---
+++ .nvim.lua
@@ -0,0 +1,39 @@
+require('telescope').setup({
+    defaults = {
+        vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+            '--hidden',
+
+            -- Exclude some patterns from search
+            "--glob=!**/.git/*",
+            "--glob=!**/.idea/*",
+            "--glob=!**/.vscode/*",
+            "--glob=!**/build/*",
+            "--glob=!**/dist/*",
+            "--glob=!**/yarn.lock",
+            "--glob=!**/package-lock.json",
+        },
+    },
+    pickers = {
+        find_files = {
+            find_command = {
+                'rg',
+                '--files',
+                '--hidden',
+                "--glob=!**/.git/*",
+                "--glob=!**/.idea/*",
+                "--glob=!**/.vscode/*",
+                "--glob=!**/build/*",
+                "--glob=!**/dist/*",
+                "--glob=!**/yarn.lock",
+                "--glob=!**/package-lock.json",
+            },
+        },
+    },
+})
