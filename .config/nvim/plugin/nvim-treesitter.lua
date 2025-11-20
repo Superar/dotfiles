155fe6dfe6314712e803ee1f271a4d1f55989fdb
Marcio Lima <marcio.lima.inacio@gmail.com>
2025-11-20 10:34:54 +0000

    Automatically install treesitter

--- .config/nvim/plugin/nvim-treesitter.lua
+++ .config/nvim/plugin/nvim-treesitter.lua
@@ -1,5 +1,6 @@
 require('nvim-treesitter.configs').setup({
     ensure_installed = {'python', 'lua', 'latex'}, -- Install parsers
+    auto_install = true,
     highlight = {
         enable = true, -- Enable syntax highlighting
     },
