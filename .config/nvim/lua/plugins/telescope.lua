9e6c16162e9fe682d1255cfa0e0aea2df68fcaa8
Marcio Lima Inacio <marcio.lima.inacio@gmail.com>
2026-06-29 19:05:33 -0300

    Fix duplicated dependency for telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,11 @@
 -- Fuzzy file finder
 return {
-    "nvim-lua/plenary.nvim", -- Needed for telescope
-    "nvim-telescope/telescope.nvim", version = "*",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    {
+        "nvim-telescope/telescope.nvim",
+        version = "*",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        }
     }
 }
