a4996746e6ed2162d6f4ded2b59d19e544ce7ba3
Marcio Lima Inacio <marcio.lima.inacio@gmail.com>
2026-09-03 10:57:09 -0300

    Add spellcheck for typst

--- .config/nvim/after/ftplugin/typst.lua
+++ .config/nvim/after/ftplugin/typst.lua
@@ -1,6 +1,9 @@
 vim.bo.commentstring = "// %s"
 vim.bo.makeprg = "make"
 
+vim.opt_local.spell = true
+vim.opt_local.spalllang = "en_us"
+
 local function tinymist_pin(path)
     local client = vim.lsp.get_clients({ name = "tinymist", bufnr = 0 })[1]
 
